a4b7afbd121641636cf65f19687fed3550f7d8ad
Mehmet Ozturk <m.ozturk@psytech.com>
2018-01-27 22:27:21 +0100

    An Update

--- Controllers/FacebookController.cs
+++ Controllers/FacebookController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Mastersi.Visualization.Models;
+using Facebook;
+using System.Net;
 
 namespace Mastersi.Visualization.Controllers
 {
@@ -14,12 +16,34 @@ namespace Mastersi.Visualization.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return Json("Selam");
+            string AppId = "395268280894053";
+            string AppSecret = "b2972f3b132cf38e80fb16c4bfa3ac18";
+            string usalFB = "universidaddesalamanca";
+
+            var client = new WebClient();
+
+            var fbClient = new FacebookClient("EAACEdEose0cBADaIuMGGB74qPvums4KOEWDUBPnGaV9Hkw3KRbZCWQX0juqZC5kcNKs4ZB9UZAeRZAwi9hv72veXqB5y2x9DXTexZCeEpz4Br6WNJKl0l5KTuZCmvlqDmzjzFUOs1N7Jjqhc5t6c17kTa3BdPU6QpLr1rlP8y4hGlD01fdlV92MZB3cyf0SBokgZD");
+
+            fbClient.AppId = AppId;
+            fbClient.AppSecret = AppSecret;
+
+            var deneme = fbClient.Get("v2.11/universidaddesalamanca");
+
+
+
+            string oauthUrl = string.Format("https://graph.facebook.com/oauth/access_token?type=client_cred&client_id={0}&client_secret={1}", "appid", "appsecret");
+
+            string accessToken = client.DownloadString(oauthUrl).Split('=')[1];
+
+            string pageInfo = client.DownloadString(string.Format("https://graph.facebook.com/wikipedia?access_token={0} ", accessToken));
+            string pagePosts = client.DownloadString(string.Format("https://graph.facebook.com/wikipedia/posts?access_token={0} ", accessToken));
+
+
+            return Json(deneme.ToString() + "ok");
+
+
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
